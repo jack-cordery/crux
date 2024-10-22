516551631c76f0c7660000b54b2e0bd4a55f237d
Jack Cordery <jack.redolf@gmail.com>
2024-10-22 10:43:50 +0100

    feat(section-divider): added animation

--- src/components/ui/section-divider.tsx
+++ src/components/ui/section-divider.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function SectionDivider() {
   return (
-    <div className="my-10 hidden h-16 w-1 rounded-full bg-gray-200 dark:bg-gray-800 sm:block"></div>
+    <div className="my-10 hidden h-16 w-1 rounded-full bg-gray-200 transition-all delay-200 duration-300 animate-in fade-in dark:bg-gray-800 sm:block"></div>
   );
 }
