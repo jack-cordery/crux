63eba8c78b20d0d537bb2746959e381579075d6d
Jack Cordery <jack.redolf@gmail.com>
2024-10-25 14:21:20 +0100

    refactor(navbar): made the navbar look way cooler

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -20,16 +20,16 @@ export default function Header() {
         </h1>
       </div>
       <div
-        className="fixed top-0 h-[4.5rem] w-full rounded-none border border-white/40 bg-white/30 shadow-lg shadow-black/[0.03]  backdrop-blur-[0.5rem] duration-500 animate-in slide-in-from-top dark:border-black/40 dark:bg-gray-950/30 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        className="fixed top-0 h-[4.5rem] w-full rounded-none border border-white/40 bg-black shadow-lg shadow-black/[0.03]  backdrop-blur-[0.5rem] duration-500 animate-in slide-in-from-top dark:border-black/40 dark:bg-gray-950 sm:top-6 sm:h-[3.25rem] sm:w-[39rem] sm:rounded-full"
       >
         <nav className="fixed left-1/2 flex h-12 -translate-x-1/2 py-2 sm:h-[initial] sm:py-0">
-          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-5 gap-y-1 text-gray-500 sm:w-[initial] sm:flex-nowrap md:gap-5">
+          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-5 gap-y-1 text-slate-200 dark:text-gray-400 sm:w-[initial] sm:flex-nowrap md:gap-5">
             {links.map(link => (
               <li className="flex h-3/4 items-center" key={link.hash}>
                 <Link
                   className={clsx(
-                    'relative flex w-full items-center justify-center p-3 transition hover:text-gray-950 dark:hover:text-gray-50',
-                    { 'text-gray-950 dark:text-gray-50': activeSection === link.name },
+                    'relative flex w-full items-center justify-center p-3 transition hover:scale-110 hover:text-white dark:hover:text-gray-50',
+                    { 'text-white dark:text-gray-50': activeSection === link.name },
                   )}
                   href={link.hash}
                   onClick={() => {
@@ -40,7 +40,7 @@ export default function Header() {
                   {link.name}
                   {link.name === activeSection && (
                     <motion.span
-                      className="absolute inset-0 -z-10 m-1 rounded-full bg-gray-300/50 dark:bg-gray-700/50"
+                      className="absolute inset-0 -z-10 m-1 rounded-full bg-slate-800 dark:bg-gray-700/50"
                       layoutId="activeSection"
                       transition={{ type: 'spring', stiffness: 380, damping: 30 }}
                     />
